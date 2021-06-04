a56a407d36322ee5a26899998a460e8142b975f5
m-knyshkova <m-knyshkova@mail.ru>
2021-06-05 01:29:08 +0300

    creater: manage with keyboard

--- js/main.js
+++ js/main.js
@@ -3,19 +3,15 @@ const swiper = new Swiper('.swiper-container', {
   //direction: 'vertical',
   loop: true,
 
-  // If we need pagination
-  //pagination: {
-    //el: '.swiper-pagination',
-  //},
-
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
-
-  // And if we need scrollbar
-  //scrollbar: {
-    //el: '.swiper-scrollbar',
-  //},
+  
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
 });
